73ff1836efb09e0d4b4a397a26c6d52b64d7c0ab
Eddie Hua <eddiehua@gmail.com>
2020-09-08 10:14:57 +0800

    只是一些嘗試

--- SoftFIDO2/SoftFido2UserClient.cpp
+++ SoftFIDO2/SoftFido2UserClient.cpp
@@ -15,24 +15,31 @@
 
 bool SoftFido2UserClient::init() {
     os_log(OS_LOG_DEFAULT, LOG_PREFIX "init");
-    return true;
+    return super::init();
+    //return true;
 }
 
 void SoftFido2UserClient::free() {
     os_log(OS_LOG_DEFAULT, LOG_PREFIX "free");
+    super::free();
 }
 
 kern_return_t IMPL(SoftFido2UserClient, Start) {
     kern_return_t ret = kIOReturnSuccess;
     ret = Start(provider, SUPERDISPATCH);
-    os_log(OS_LOG_DEFAULT, LOG_PREFIX "Start");
+    if (ret != kIOReturnSuccess) {
+        os_log(OS_LOG_DEFAULT, LOG_PREFIX "Start Failed!");
+    } else {
+        os_log(OS_LOG_DEFAULT, LOG_PREFIX "Start");
+    }
     return ret;
 }
 
 kern_return_t IMPL(SoftFido2UserClient, Stop) {
-    kern_return_t ret = kIOReturnSuccess;
+    //kern_return_t ret = kIOReturnSuccess;
     os_log(OS_LOG_DEFAULT, LOG_PREFIX "Stop");
-    return ret;
+    return Stop(provider, SUPERDISPATCH);
+    //return ret;
 }
 
 kern_return_t SoftFido2UserClient::ExternalMethod(uint64_t selector,
@@ -43,5 +50,13 @@ kern_return_t SoftFido2UserClient::ExternalMethod(uint64_t selector,
     
     kern_return_t ret = kIOReturnSuccess;
     os_log(OS_LOG_DEFAULT, LOG_PREFIX "ExternalMethod");
+
+    IOService* client;
+
+    ret = Create(this, "Fido2HidProperties", &client);
+    if (ret != kIOReturnSuccess) {
+        os_log(OS_LOG_DEFAULT, LOG_PREFIX "IOService::Create failed: 0x%x", ret);
+        return ret;
+    }
     return ret;
 }
