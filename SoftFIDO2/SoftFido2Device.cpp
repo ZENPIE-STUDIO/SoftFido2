f9ba382146dddcb6177940584ae95c2338a08d2a
Eddie Hua <eddiehua@gmail.com>
2020-09-25 17:06:56 +0800

    備份

--- SoftFIDO2/SoftFido2Device.cpp
+++ SoftFIDO2/SoftFido2Device.cpp
@@ -15,6 +15,7 @@
 #include <HIDDriverKit/IOHIDInterface.h>
 #include <HIDDriverKit/IOHIDDeviceKeys.h>
 #include <HIDDriverKit/IOHIDUsageTables.h>
+#include <USBDriverKit/IOUSBHostInterface.h>
 #include "u2f_hid.h"
 #include "UserKernelShared.h"
 #include "SoftFido2Device.h"
@@ -24,8 +25,8 @@
 #define LOG_PREFIX "[SoftFido2][Device] "
 
 struct SoftFido2Device_IVars {
-    SoftFido2UserClient* provider;
-    
+    SoftFido2UserClient*    provider;
+    IOUSBHostInterface*     interface;
 };
 
 bool SoftFido2Device::init() {
@@ -137,25 +138,24 @@ kern_return_t SoftFido2Device::setReport(IOMemoryDescriptor* report,
     } else {
         os_log(OS_LOG_DEFAULT, LOG_PREFIX "_CopyState failed = %d", ret);
     }
-    // 用 Map, CreateMapping 都失敗
-    /*
-//    uint64_t address;
-//    uint64_t len;
+    // 實驗：IOBufferMemoryDescriptor 建立時，無論用什麼參數 In/Out，都可 Map address = 4439891968
+    // (怪) 用 Map, CreateMapping 都失敗
+    //uint64_t address;
+    uint64_t length;
     IOMemoryMap* map = nullptr;
-    ret = report->CreateMapping(kIOMemoryMapCacheModeDefault, 0, 0, 64, 0, &map);
-    //ret = report->Map(0, 0, 0, 0, &address, &len);
+    ret = report->CreateMapping(kIOMemoryMapReadOnly, 0, 0, 0, 0, &map);
+    //ret = report->Map(0, 0, 0, 0, &address, &length);
     if (ret == kIOReturnSuccess) {
-//        os_log(OS_LOG_DEFAULT, LOG_PREFIX "address = %llu", address);
-//        os_log(OS_LOG_DEFAULT, LOG_PREFIX "len = %llu", len);
+        //os_log(OS_LOG_DEFAULT, LOG_PREFIX "address = %llu", address);
+        //os_log(OS_LOG_DEFAULT, LOG_PREFIX "length = %llu", length);
         os_log(OS_LOG_DEFAULT, LOG_PREFIX "address = %llu", map->GetAddress());
-        os_log(OS_LOG_DEFAULT, LOG_PREFIX "len = %llu", map->GetLength());
+        os_log(OS_LOG_DEFAULT, LOG_PREFIX "length = %llu", map->GetLength());
     } else {
-        os_log(OS_LOG_DEFAULT, LOG_PREFIX "report->CreateMapping failed = %d", ret);
+        os_log(OS_LOG_DEFAULT, LOG_PREFIX "report->Map failed = %d", ret);
         os_log(OS_LOG_DEFAULT, LOG_PREFIX "       system err = %x", err_get_system(ret));
         os_log(OS_LOG_DEFAULT, LOG_PREFIX "          sub err = %x", err_get_sub(ret));
         os_log(OS_LOG_DEFAULT, LOG_PREFIX "         code err = %x", err_get_code(ret));
-    }*/
-    //os_log(OS_LOG_DEFAULT, LOG_PREFIX "setReport Report GetClassName = %s", report->GetClassName());
+    }
     // 用 User Client 去處理接收到的資料
     SoftFido2UserClient *userClient = ivars->provider;
     if (userClient != nullptr) {
@@ -163,7 +163,6 @@ kern_return_t SoftFido2Device::setReport(IOMemoryDescriptor* report,
     }
     // Sleep for a bit to make the HID conformance tests happy.
     os_log(OS_LOG_DEFAULT, LOG_PREFIX "setReport - CompleteReport");
-    uint64_t length = 0;
     report->GetLength(&length);
     CompleteReport(action, kIOReturnSuccess, (uint32_t) length); // 沒有這個，LOG會有
     // SoftFido2Device:0x1000008a6 Action aborted 0 1
@@ -186,8 +185,8 @@ bool SoftFido2Device::handleStart(IOService* provider) {
         os_log(OS_LOG_DEFAULT, LOG_PREFIX "handleStart > super::handleStart failed");
         return false;
     }
-    //ivars->provider-> 不用 setReady
-    
+    // IOUSBHostInterface 可以幹什麼？
+    //ivars->interface = OSDynamicCast(IOUSBHostInterface, provider);
     return true;
 }
 
@@ -246,9 +245,9 @@ OSDictionary* SoftFido2Device::newDeviceDescription(void) {
 }
 
 OSData* SoftFido2Device::newReportDescriptor(void) {
-    const size_t kSizeOfReportDescriptor = sizeof(kFido2HidReportDescriptor);
+    const size_t kSizeOfReportDescriptor = sizeof(u2fhid_report_descriptor);
     os_log(OS_LOG_DEFAULT, LOG_PREFIX "newReportDescriptor size = %lu", kSizeOfReportDescriptor);
-    return OSData::withBytes(kFido2HidReportDescriptor, kSizeOfReportDescriptor);
+    return OSData::withBytes(u2fhid_report_descriptor, kSizeOfReportDescriptor);
 }
 
 #pragma mark - 備份一些測試function
--- SoftFIDO2/UserKernelShared.h
+++ SoftFIDO2/UserKernelShared.h
@@ -21,26 +21,26 @@ enum {
 
 // Eddie 參考 HID_ReportDesc_fido
 // https://github.com/LedgerHQ/nanos-secure-sdk/blob/master/lib_stusb_samples/Class/HID%2BFIDO/usbd_hid_impl.c
-unsigned char const kFido2HidReportDescriptor[] = {
-    0x06, 0xD0, 0xF1, // Usage Page (Reserved 0xF1D0)
-    0x09, 0x01,       // Usage (0x01)
-    0xA1, 0x01,       // Collection (Application)
-    // The Input report
-    0x09, 0x03,       // Usage ID - Vendor defined
-    0x15, 0x00,       //   Logical Minimum (0)
-    0x26, 0xFF, 0x00, //   Logical Maximum (255)
-    0x75, 0x08,       //   Report Size (8)
-    0x95, 0x40,       //   Report Count (64)
-    0x81, 0x08,       //   Input (Data,Var,Abs)
-    // The Output report
-    0x09, 0x04,       // Usage ID - Vendor defined
-    0x15, 0x00,       //   Logical Minimum (0)
-    0x26, 0xFF, 0x00, //   Logical Maximum (255)
-    0x75, 0x08,       //   Report Size (8)
-    0x95, 0x40,       //   Report Count (64)
-    0x91, 0x08,       //   Output (Data,Var,Abs)
-    0xC0,             // End Collection
-};
+//unsigned char const kFido2HidReportDescriptor[] = {
+//    0x06, 0xD0, 0xF1, // Usage Page (Reserved 0xF1D0)
+//    0x09, 0x01,       // Usage (0x01)
+//    0xA1, 0x01,       // Collection (Application)
+//    // The Input report
+//    0x09, 0x03,       // Usage ID - Vendor defined
+//    0x15, 0x00,       //   Logical Minimum (0)
+//    0x26, 0xFF, 0x00, //   Logical Maximum (255)
+//    0x75, 0x08,       //   Report Size (8)
+//    0x95, 0x40,       //   Report Count (64)
+//    0x81, 0x08,       //   Input (Data,Var,Abs)
+//    // The Output report
+//    0x09, 0x04,       // Usage ID - Vendor defined
+//    0x15, 0x00,       //   Logical Minimum (0)
+//    0x26, 0xFF, 0x00, //   Logical Maximum (255)
+//    0x75, 0x08,       //   Report Size (8)
+//    0x95, 0x40,       //   Report Count (64)
+//    0x91, 0x08,       //   Output (Data,Var,Abs)
+//    0xC0,             // End Collection
+//};
 
 // FIDO2 (Ming)
 //const uint8_t kFido2HidReportDescriptor[] = {
@@ -65,25 +65,25 @@ unsigned char const kFido2HidReportDescriptor[] = {
 //};
 
 // 原本 SoftU2F 的
-//uint8_t const u2fhid_report_descriptor[] = {
-//    0x06, 0xD0, 0xF1, // Usage Page (FIDO Alliance : Reserved 0xF1D0)
-//    0x09, 0x01,       // Usage (U2F HID Auth. Device : 0x01)
-//    0xA1, 0x01,       // Collection (Application)
-//    0x09, 0x20,       //   Usage (Input Report Data: 0x20)
-//    0x15, 0x00,       //   Logical Minimum (0)
-//    0x26, 0xFF, 0x00, //   Logical Maximum (255)
-//    0x75, 0x08,       //   Report Size (8)
-//    0x95, 0x40,       //   Report Count (64)
-//    0x81, 0x02,       //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null
-//                      //   Position)
-//    0x09, 0x21,       //   Usage (Output Report Data: 0x21)
-//    0x15, 0x00,       //   Logical Minimum (0)
-//    0x26, 0xFF, 0x00, //   Logical Maximum (255)
-//    0x75, 0x08,       //   Report Size (8)
-//    0x95, 0x40,       //   Report Count (64)
-//    0x91, 0x02,       //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null
-//                      //   Position,Non-volatile)
-//    0xC0,             // End Collection
-//};
+uint8_t const u2fhid_report_descriptor[] = {
+    0x06, 0xD0, 0xF1, // Usage Page (FIDO Alliance : Reserved 0xF1D0)
+    0x09, 0x01,       // Usage (U2F HID Auth. Device : 0x01)
+    0xA1, 0x01,       // Collection (Application)
+    0x09, 0x20,       //   Usage (Input Report Data: 0x20)
+    0x15, 0x00,       //   Logical Minimum (0)
+    0x26, 0xFF, 0x00, //   Logical Maximum (255)
+    0x75, 0x08,       //   Report Size (8)
+    0x95, 0x40,       //   Report Count (64)
+    0x81, 0x02,       //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null
+                      //   Position)
+    0x09, 0x21,       //   Usage (Output Report Data: 0x21)
+    0x15, 0x00,       //   Logical Minimum (0)
+    0x26, 0xFF, 0x00, //   Logical Maximum (255)
+    0x75, 0x08,       //   Report Size (8)
+    0x95, 0x40,       //   Report Count (64)
+    0x91, 0x02,       //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null
+                      //   Position,Non-volatile)
+    0xC0,             // End Collection
+};
 
 #endif /* UserKernelShared_h */
