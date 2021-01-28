f28ad65d38672ac11904021e894da787c48cd8eb
Eddie Hua <eddiehua@gmail.com>
2021-01-28 16:22:51 +0800

    New Function Test Known Issues：⚠️When receiving HID Frame, data loss will occur. ⚠️

--- UserClient/ViewController.m
+++ UserClient/ViewController.m
@@ -8,24 +8,48 @@
 
 #import "ViewController.h"
 #import "U2FHID.h"
+#include <IOKit/hid/IOHIDManager.h>
 
 @interface ViewController()<U2fHidDelegate>
 @property (nonatomic) U2FHID* u2fhid;
+
+// Try SetReport to SoftFIDO2 Driver
+@property (nonatomic) NSButton* btnSetReport;
+//
+@property (nonatomic) IOHIDManagerRef hidManager;
+@property (nonatomic) io_iterator_t addedIter;
+@property (nonatomic) IOHIDDeviceRef softu2fDevice;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSTextField* textfield = [[NSTextField alloc] init];
+    textfield.alignment = NSTextAlignmentCenter;
+    textfield.editable = NO;
+    textfield.bordered = NO;
+    textfield.bezeled = NO;
+    //textfield.textColor = newColorRGB(40, 40, 40);
+    textfield.font = [NSFont systemFontOfSize:20];
+    textfield.backgroundColor = [NSColor clearColor];
+    textfield.stringValue = @"Here will be no information, see your xcode log.";
+    [textfield sizeToFit];
+    NSRect frame = textfield.frame;
+    frame.origin.y = CGRectGetMidY(self.view.frame);
+    frame.size.width = self.view.frame.size.width;
+    textfield.frame = frame;
+    [self.view addSubview:textfield];
 
     // Do any additional setup after loading the view.
     [self startFidoDriverUserClient];
+    //
+    [self initDriverTester];
 }
 
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
-
     // Update the view, if already loaded.
 }
 
@@ -53,6 +77,8 @@
         return ok;
     }];
     NSLog(@"FidoDriverUserClient Start OK ✅");
+    NSLog(@"To view the Log, enter the command in the console:");
+    NSLog(@"$ log show --predicate 'sender == \"sysextd\" or sender CONTAINS[c] \"Fido\" or sender CONTAINS[c] \"HID\"' --info --debug --last 2m");
 }
 
 #pragma mark - U2fHidDelegate
@@ -63,4 +89,154 @@
         NSLog(@"didStopU2FHID Stop");
     }
 }
+
+
+#pragma mark - Test SoftFIDO2 Driver Known Issues
+
+- (void) initDriverTester {
+    _btnSetReport = [NSButton buttonWithTitle:@"Try SetReport" target:self action:@selector(trySetReport)];
+    [_btnSetReport sizeToFit];
+    NSRect frame = _btnSetReport.frame;
+    frame.origin.x = 10;
+    frame.origin.y = 10;
+    _btnSetReport.frame = frame;
+    [self.view addSubview:_btnSetReport];
+    // Do any additional setup after loading the view.
+    [self initHIDManager];
+}
+
++ (NSModalResponse) alertMessage:(NSString* _Nonnull) message Info:(NSString* _Nullable) info ButtonTitle:(NSString* _Nonnull) buttonTitle {
+    NSAlert* alert = [[NSAlert alloc] init];
+    alert.messageText = message;
+    if (info != nil)
+        alert.informativeText = info;
+    [alert addButtonWithTitle:buttonTitle];
+    return [alert runModal];
+}
+// ⚠️When receiving HID Frame, data loss will occur. ⚠️
+- (void) trySetReport {
+    if (_softu2fDevice == nil) {
+        NSLog(@"softu2fDevice is nil");
+        NSAlert* alert = [[NSAlert alloc] init];
+        alert.messageText = @"SoftFIDO2 Driver not work.";
+        [alert runModal];
+        return;
+    }
+    
+    IOReturn ret = IOHIDDeviceOpen(_softu2fDevice, kIOHIDOptionsTypeNone );
+    NSLog(@"IOHIDDeviceOpen ret = 0x%X", ret);  // kIOReturnNotPermitted (2E2)
+    ViewController* __weak weakSelf = self;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        const CFIndex reportLength = 64;
+        uint8_t report[reportLength] = {0x0};
+        // 連續送個10次
+        for (int i = 1; i < 10; i++) {
+            memset(report, i, reportLength);
+            IOReturn ret = IOHIDDeviceSetReport(weakSelf.softu2fDevice, kIOHIDReportTypeOutput,
+                                       0, // CFIndex reportID
+                                       report, reportLength);
+            NSLog(@"IOHIDDeviceSetReport %d  ret = 0x%X", i, ret);
+            if (ret != 0) {
+                NSLog(@"       system err = %x", err_get_system(ret));
+                NSLog(@"          sub err = %x", err_get_sub(ret));
+                NSLog(@"         code err = %x", err_get_code(ret));
+                break;
+            }
+            // kIOReturnNotOpen (2CD)
+            // kIOReturnTimeout (2D6) <= After updated macOS 11.1 Beta (20C5048k)
+            [NSThread sleepForTimeInterval:1.1];
+        }
+    });
+//    IOHIDDeviceClose(_softu2fDevice, kIOHIDOptionsTypeNone);
+}
+#pragma mark -
+// --------------------------------------------
+void onDeviceAddedCallback(void * _Nullable        context,
+                           IOReturn                result,
+                           void * _Nullable        sender,
+                           IOHIDDeviceRef          device) {
+    //NSLog(@"--------------- onDeviceAdded ---------------");
+    ViewController* vc = (__bridge ViewController*) context;
+    //[vc dumpDeviceInfo:device];
+    // -----
+    NSString* productName = getStringProperty(device, CFSTR(kIOHIDProductKey), @"");
+    if ([productName isEqualToString:@"SoftFido2Driver (DriverKit)"]) {
+//    if ([productName isEqualToString:@"SoftU2F"]) {
+        vc.softu2fDevice = device;
+    }
+}
+void onDeviceRemovedCallback(void * _Nullable        context,
+                             IOReturn                result,
+                             void * _Nullable        sender,
+                             IOHIDDeviceRef          device) {
+    //NSLog(@"--------------- onDeviceRemoved ---------------");
+    //ViewController* vc = (__bridge ViewController*) context;
+    //[vc dumpDeviceInfo:device];
+}
+
+int32_t getIntProperty(IOHIDDeviceRef device, CFStringRef key, int32_t defaultValue) {
+    int32_t value = 0;
+    CFNumberRef number = IOHIDDeviceGetProperty(device, key);
+    if (number != NULL && CFNumberGetValue(number, kCFNumberSInt32Type, &value)) {
+    } else {
+        value = defaultValue;
+    }
+    return value;
+}
+
+NSString* getStringProperty(IOHIDDeviceRef device, CFStringRef key, NSString* defaultValue) {
+    //CFStringRef value = defaultValue;
+    CFStringRef value = IOHIDDeviceGetProperty(device, key);
+    if (value == NULL) {
+        value = (__bridge CFStringRef) defaultValue;
+    }
+    return (__bridge NSString*) value;
+}
+#pragma mark - IOHIDManager
+- (void) initHIDManager {
+    _hidManager = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
+    IOHIDManagerSetDeviceMatching(_hidManager, NULL);
+    IOHIDManagerRegisterDeviceMatchingCallback(_hidManager, onDeviceAddedCallback, (void*)self);
+    IOHIDManagerRegisterDeviceRemovalCallback(_hidManager,
+          onDeviceRemovedCallback, (void*)self);
+    IOHIDManagerScheduleWithRunLoop(_hidManager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+    // ---
+}
+
+- (void) releaseHIDManager {
+    if (_hidManager) {
+        IOHIDManagerUnscheduleFromRunLoop(_hidManager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+        IOHIDManagerClose(_hidManager, kIOHIDOptionsTypeNone);
+        CFRelease(_hidManager);
+        _hidManager = NULL;
+    }
+}
+
+
+- (void) dumpDeviceInfo:(IOHIDDeviceRef) device {
+    int32_t locationId = getIntProperty(device, CFSTR(kIOHIDLocationIDKey), 0);
+    int32_t vendorId = getIntProperty(device, CFSTR(kIOHIDVendorIDKey), 0);
+    int32_t productId = getIntProperty(device, CFSTR(kIOHIDProductIDKey), 0);
+    int32_t usage = getIntProperty(device, CFSTR(kIOHIDPrimaryUsageKey), 0);
+    int32_t usage_page = getIntProperty(device, CFSTR(kIOHIDPrimaryUsagePageKey), 0);
+    int32_t maxInputReportSize = getIntProperty(device, CFSTR(kIOHIDMaxInputReportSizeKey), 0);
+    int32_t maxOutputReportSize = getIntProperty(device, CFSTR(kIOHIDMaxOutputReportSizeKey), 0);
+    int32_t maxFeatureReportSize = getIntProperty(device, CFSTR(kIOHIDMaxFeatureReportSizeKey), 0);
+    //
+    NSString* productName = getStringProperty(device, CFSTR(kIOHIDProductKey), @"");
+    NSString* serialNumber = getStringProperty(device, CFSTR(kIOHIDSerialNumberKey), @"");
+    NSString* manufacturer = getStringProperty(device, CFSTR(kIOHIDManufacturerKey), @"");
+    NSLog(@"productName = %@", productName);
+    NSLog(@"serialNumber = %@", serialNumber);
+    NSLog(@"manufacturer = %@", manufacturer);
+    NSLog(@"LocationID = %d", locationId);
+    NSLog(@"vendorId = %d", vendorId);
+    NSLog(@"productId = %d", productId);
+    NSLog(@"usage = %d", usage);
+    NSLog(@"usage_page = %d", usage_page);
+    NSLog(@"maxInputReportSize = %d", maxInputReportSize);
+    NSLog(@"maxOutputReportSize = %d", maxOutputReportSize);
+    NSLog(@"maxFeatureReportSize = %d", maxFeatureReportSize);
+}
+
 @end
